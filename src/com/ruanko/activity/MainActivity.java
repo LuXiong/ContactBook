b93c6ddd95306ac61dd41c19d753bb3967937388
LuXiong <LuXiong@github.com>
2015-01-08 19:43:02 +0800

    some change

--- src/com/ruanko/activity/MainActivity.java
+++ src/com/ruanko/activity/MainActivity.java
@@ -1,23 +1,18 @@
 package com.ruanko.activity;
 
-import java.util.List;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.Toast;
 
-
-import com.ruanko.adapter.UserAdapter;
 import com.ruanko.bussiness.UserBussiness;
 import com.ruanko.contactbook.BaseActivity;
 import com.ruanko.contactbook.R;
-import com.ruanko.listener.DataBaseListener;
-import com.ruanko.model.User;
+//import com.ruanko.adapter.UserAdapter;
+
+//import com.ruanko.model.User;
 
 public class MainActivity extends BaseActivity {
 	private ListView listView;
@@ -26,62 +21,37 @@ public class MainActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-		listView = (ListView)this.findViewById(R.id.listView);
+		listView = (ListView) this.findViewById(R.id.listView);
 		listView.setOnItemClickListener(new ItemClickListener());
-		show();
-		
-		UserBussiness ub= new UserBussiness();
-		ub.addName(new DataBaseListener(){
+		// show();
 
-			@Override
-			public void onStart() {
-				// TODO Auto-generated method stub
-				super.onStart();
-			}
-
-			@Override
-			public void onSuccess() {
-				// TODO Auto-generated method stub
-				super.onSuccess();
-			}
-
-			@Override
-			public void onFinish() {
-				// TODO Auto-generated method stub
-				super.onFinish();
-			}
-
-			@Override
-			public void onFailure() {
-				// TODO Auto-generated method stub
-				super.onFailure();
-			}
-			
-		});
+		UserBussiness ub = new UserBussiness();
 	}
-	private final class ItemClickListener implements OnItemClickListener{
+
+	private final class ItemClickListener implements OnItemClickListener {
 
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
-			ListView lView = (ListView)parent;//页面
-			User user = (User)lView.getItemAtPosition(position);
-	//		Toast.makeText(getApplicationContext(), user.getId().toString(), 1).show();
-			
+			ListView lView = (ListView) parent;// 页面
+			// User user = (User)lView.getItemAtPosition(position);
+			// Toast.makeText(getApplicationContext(), user.getId().toString(),
+			// 1).show();
+
 		}
-		
+
 	}
-//自定义适配器
-	private void show() {
-		List<User> users = null;
-		UserAdapter adapter = new UserAdapter(this,users,R.layout.item);
-		
-		
-=======
-		Log.v("wq","hi contactbook");
->>>>>>> df47959676b0d7725118996bbbad1e6c765345f1
-	}
+
+	// 自定义适配器
+	// private void show() {
+	// List<User> users = null;
+	// UserAdapter adapter = new UserAdapter(this,users,R.layout.item);
+	//
+	//
+	// =======
+	// Log.v("wq","hi contactbook");
+	// >>>>>>> df47959676b0d7725118996bbbad1e6c765345f1
+	// }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/ruanko/bussiness/UserBussiness.java
+++ src/com/ruanko/bussiness/UserBussiness.java
@@ -1,12 +1,16 @@
 package com.ruanko.bussiness;
 
+import java.util.ArrayList;
+
+import android.content.Context;
+
 import com.ruanko.listener.DataBaseListener;
+import com.ruanko.model.Contact;
 
 public class UserBussiness {
-
-	public void addName(DataBaseListener dataBaseListener) {
-		// TODO Auto-generated method stub
+	public ArrayList<Contact> loadContacts(Context context, DataBaseListener listener){
+		ArrayList<Contact> contacts = new ArrayList<Contact>();
 		
+		return null;
 	}
-
 }
