8ae629f1781b9c8bcc51af9d3d646784e1d3a236
LuXiong <LuXiong@github.com>
2015-01-14 15:07:01 +0800

    hello

--- src/com/ruanko/activity/EditActivity.java
+++ src/com/ruanko/activity/EditActivity.java
@@ -4,36 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.ScrollView;
 
 import com.ruanko.contactbook.BaseActivity;
 import com.ruanko.contactbook.R;
 
 public class EditActivity extends BaseActivity {
-	
+
 	private ScrollView scrollView;
 	private MenuItem item;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_editor);
 		setOnRightBtnClickListener(new OnRightBtnClickListener() {
-			
+
 			@Override
 			public void onClick(MenuItem item) {
-				 Intent intent = new Intent();
-                 intent.setClass(EditActivity.this, DetailActivity.class);
-                 startActivity(intent);
-                 EditActivity.this.finish();
+				Intent intent = new Intent();
+				intent.setClass(EditActivity.this, DetailActivity.class);
+				startActivity(intent);
+				EditActivity.this.finish();
 			}
 		});
-		scrollView = (ScrollView)this.scrollView;
+		scrollView = (ScrollView) this.scrollView;
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		boolean result = super.onCreateOptionsMenu(menu);
@@ -41,7 +40,5 @@ public class EditActivity extends BaseActivity {
 		hideSearchBtn();
 		return result;
 	}
-	
 
-	
 }
