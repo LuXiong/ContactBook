982d52d7041300a5ad1ab3dd47ea8ab3042eb19c
LuXiong <LuXiong@github.com>
2015-01-16 16:41:10 +0800

    code sort

--- src/com/ruanko/activity/DetailActivity.java
+++ src/com/ruanko/activity/DetailActivity.java
@@ -1,11 +1,10 @@
 package com.ruanko.activity;
 
-import java.util.ArrayList;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,130 +13,123 @@ import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.ruanko.bussiness.ContactBussiness;
 import com.ruanko.contactbook.BaseActivity;
 import com.ruanko.contactbook.R;
-import com.ruanko.contactbook.BaseActivity.OnRightBtnClickListener;
 import com.ruanko.model.Contact;
 
 public class DetailActivity extends BaseActivity {
-	
-	final static public String EXTRA_INPUT = "contact"; 
-	
-	private ScrollView scrollView;
-	private ArrayList<Contact> mContact;
-	
-	private ImageButton mBtnCall;
-	private ImageButton mBtnMessage;
-    private ImageButton mBtnDel;
-    
+
+	final static public String EXTRA_INPUT = "contact";
+
+	private Contact mContact;
+
+	private ScrollView mScrollView;
+	private ImageButton mCallBtn, mMessageBtn, mDelBtn;
+	private TextView mNameTextView, mPhoneTextView, mAddrTextView,
+			mEmailTextView;
+
 	@Override
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
-		setContentView(R.layout.activity_details);   
+		setContentView(R.layout.activity_details);
 		findView();
 		initView();
-		
+		loadData();
 	}
 
-	
+	private void loadData() {
+		mContact = (Contact) getIntent().getSerializableExtra(
+				DetailActivity.EXTRA_INPUT);
+		notifyDatasetChanged();
+	}
+
 	private void findView() {
-		scrollView = (ScrollView)this.findViewById(R.id.ScrollView1);
-		
+		mScrollView = (ScrollView) findViewById(R.id.activity_detail_scrollView);
+		mCallBtn = (ImageButton) findViewById(R.id.activity_detail_callBtn);
+		mMessageBtn = (ImageButton) findViewById(R.id.activity_detail_messageBtn);
+		mDelBtn = (ImageButton) findViewById(R.id.activity_detail_delBtn);
+		mNameTextView = (TextView) findViewById(R.id.activity_detail_name);
+		mAddrTextView = (TextView) findViewById(R.id.activity_detail_address);
+		mEmailTextView = (TextView) findViewById(R.id.activity_detail_email);
+		mPhoneTextView = (TextView) findViewById(R.id.activity_detail_phone);
 	}
 
-    private void initView() {
-    	mContact = new ArrayList<Contact>();
-    	ContactBussiness cb = new ContactBussiness();
-    	mContact = cb.fetchContactInformation(this);
-    	mBtnCall = (ImageButton)this.findViewById(R.id.btn_call);
-    	mBtnMessage = (ImageButton)this.findViewById(R.id.btn_message);
-    	mBtnDel = (ImageButton)this.findViewById(R.id.btn_delete);
-    	mBtnCall.setOnClickListener(new OnClickListener() {
-    		@Override
-			public void onClick(View v) {
-				String phoneNumber = "15072334868";
-				Intent intent = new Intent(Intent.ACTION_CALL,Uri.parse("tel:" + phoneNumber));
-				//intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				startActivity(intent);
-			}
-		});
-    	mBtnMessage.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				String phoneNumber = "15072334868";
-				Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:" + phoneNumber));//get(position); 
-				intent.setType("vnd.android-dir/mms-sms");  
-				 // intent.setData(Uri.parse("content://mms-sms/conversations/"));//此为号码
-				startActivity(intent);
-				
-			}
-		});
-    	mBtnDel.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				new AlertDialog.Builder(DetailActivity.this) 
-				.setTitle("确认")
-				.setMessage("确定吗？")
-				.setPositiveButton("是", null)//添加删除功能或者不改变
-				.setNegativeButton("否", null)
-				.show();
-			
-			}
-		});
-		setOnRightBtnClickListener(new OnRightBtnClickListener() {
-			
-			@Override
-			public void onClick(MenuItem item) {
-				Intent intent = new Intent();
-                intent.setClass(DetailActivity.this, EditActivity.class);
-                startActivity(intent);
-				
-			}
-			});
-    	
-    	initData();
+	private void initView() {
 		bindEvent();
 	}
 
-
 	private void bindEvent() {
+		mCallBtn.setOnClickListener(new CallBtnClickListener());
+		mMessageBtn.setOnClickListener(new OnClickListener() {
 
-		
+			@Override
+			public void onClick(View v) {
+				String phoneNumber = "15072334868";
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:"
+						+ phoneNumber));// get(position);
+				intent.setType("vnd.android-dir/mms-sms");
+				// intent.setData(Uri.parse("content://mms-sms/conversations/"));//此为号码
+				startActivity(intent);
+
+			}
+		});
+		mDelBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				new AlertDialog.Builder(DetailActivity.this).setTitle("确认")
+						.setMessage("确定吗？").setPositiveButton("是", null)// 添加删除功能或者不改变
+						.setNegativeButton("否", null).show();
+
+			}
+		});
+		setOnRightBtnClickListener(new OnRightBtnClickListener() {
+
+			@Override
+			public void onClick(MenuItem item) {
+				Intent intent = new Intent();
+				intent.setClass(DetailActivity.this, EditActivity.class);
+				startActivity(intent);
+
+			}
+		});
+	}
+
+	private void notifyDatasetChanged() {
+		if(mContact!=null){
+			mNameTextView.setText(mContact.getName());
+			mAddrTextView.setText(mContact.getAddr());
+		}
+//		mPhoneTextView.setText((CharSequence) mContact.getPhones());
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	boolean result = super.onCreateOptionsMenu(menu);
-	setRightTitle("编辑");
-	hideSearchBtn();
-	return result;
-}
-
-	private void initData() {
-		//setTitle("hello world");  
-
-		TextView m_name = (TextView)this.findViewById(R.id.name);   
-		TextView m_addr = (TextView)this.findViewById(R.id.address); 
-		TextView m_email= (TextView)this.findViewById(R.id.email); 
-		TextView m_phones=(TextView)this.findViewById(R.id.phone);
-        Contact mContact = (Contact)getIntent().getSerializableExtra(DetailActivity.EXTRA_INPUT);    
-        m_name.setText(mContact.getName()); 
-        m_addr.setText(mContact.getAddr()); 
-        m_phones.setText((CharSequence) mContact.getPhones());
-     
-		
+		boolean result = super.onCreateOptionsMenu(menu);
+		setRightTitle("编辑");
+		hideSearchBtn();
+		return result;
 	}
 
 	public class LaunchScrollViewClickListener implements OnClickListener {
 
 		@Override
 		public void onClick(View v) {
-			
+
 		}
 
 	}
-	
+
+	public class CallBtnClickListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			String phoneNumber = "15072334868";
+			Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"
+					+ phoneNumber));
+			startActivity(intent);
+		}
+
+	}
+
 }
--- src/com/ruanko/activity/MainActivity.java
+++ src/com/ruanko/activity/MainActivity.java
@@ -6,16 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import com.ruanko.adapter.ContactListAdapter;
-import com.ruanko.adapter.UserAdapter;
 import com.ruanko.bussiness.ContactBussiness;
 import com.ruanko.contactbook.BaseActivity;
 import com.ruanko.contactbook.R;
@@ -26,9 +23,7 @@ import com.ruanko.model.Contact;
 public class MainActivity extends BaseActivity {
 	private ListView mListView;
 	private BaseAdapter mAdapter;
-	private OnMenuItemClickListener listner;
 	private ArrayList<ContactItemInterface> mContactList;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,71 +31,60 @@ public class MainActivity extends BaseActivity {
 		setContentView(R.layout.activity_main);
 		findView();
 		initView();
-		
-		
+		loadData();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		boolean result = super.onCreateOptionsMenu(menu);
+		setRightTitle("新建");
+		return result;
+	}
+
+	private void findView() {
+		mListView = (ContactListView) this.findViewById(R.id.activity_main_listView);
 	}
 
 	private void initView() {
-		
 		mContactList = new ArrayList<ContactItemInterface>();
-		ContactBussiness cb = new ContactBussiness();
-		mContactList.addAll( cb.fetchContactInformation(this));
 		mAdapter = new ContactListAdapter(this, mContactList);
 		mListView.setAdapter(mAdapter);
 		mListView.setFastScrollEnabled(true);
 		mListView.setOnItemClickListener(new ItemClickListener());
 		setOnRightBtnClickListener(new OnRightBtnClickListener() {
-			
+
 			@Override
 			public void onClick(MenuItem item) {
 				Intent intent = new Intent();
-                intent.setClass(MainActivity.this, EditActivity.class);
-                startActivity(intent);
-				
+				intent.setClass(MainActivity.this, EditActivity.class);
+				startActivity(intent);
 			}
-			});
-		}
-	
-		private void findView() {
-			mListView = (ContactListView) this.findViewById(R.id.listView);
-		}
-	
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-		boolean result = super.onCreateOptionsMenu(menu);
-		setRightTitle("新建");
-		return result;
+		});
 	}
-	
 
-	
-	
+	private void loadData() {
+		ContactBussiness cb = new ContactBussiness();
+		mContactList.clear();
+		mContactList.addAll(cb.fetchContactInformation(this));
+		notifyDataSetChanged();
+	}
 
 	private final class ItemClickListener implements OnItemClickListener {
-		//TextView m;
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
 			mContactList.get(position);
-			/****
-			Toast.makeText(getApplicationContext(), String.valueOf(position), 1)
-					.show();
-					****/
 			Intent intent = new Intent();
 			intent.setClass(MainActivity.this, DetailActivity.class);
-			intent.putExtra(DetailActivity.EXTRA_INPUT, (Contact)mContactList.get(position));
-			startActivity(intent);   
-	
+			intent.putExtra(DetailActivity.EXTRA_INPUT,
+					(Contact) mContactList.get(position));
+			startActivity(intent);
 		}
 
 	}
-	
-	
-	private void notifyDataSetChanged(){
+
+	private void notifyDataSetChanged() {
 		mAdapter.notifyDataSetChanged();
 	}
-
-
-
 
 }
