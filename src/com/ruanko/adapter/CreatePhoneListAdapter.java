5eb9cb926420f41818c8f43d3c0f88afd7dabbb2
zyz <1023409822@qq.com>
2015-01-19 10:20:56 +0800

    detail

--- src/com/ruanko/adapter/CreatePhoneListAdapter.java
+++
@@ -1,5 +0,0 @@
-package com.ruanko.adapter;
-
-public class CreatePhoneListAdapter {
-
-}
--- src/com/ruanko/control/ContactItemInterface.java
+++
@@ -1,8 +0,0 @@
-package com.ruanko.control;
-
-public interface ContactItemInterface {
-
-	public String getOrderingItem();
-
-	public String getDisplayItem();
-}
